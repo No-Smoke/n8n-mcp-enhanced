e326ba95afde1368085fc1a0507eb93f2998b1d6
github-action-benchmark <github@users.noreply.github.com>
2025-12-04 00:43:38 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for 3f427f952836fd35cb6d054a45d179d88b9cb37b

---
+++ benchmarks/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1764809018713,
+  "repoUrl": "https://github.com/No-Smoke/n8n-mcp-enhanced",
+  "entries": {
+    "n8n-mcp Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "56956555+czlonkowski@users.noreply.github.com",
+            "name": "Romuald Członkowski",
+            "username": "czlonkowski"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3f427f952836fd35cb6d054a45d179d88b9cb37b",
+          "message": "Update n8n to 1.117.2 (#379)",
+          "timestamp": "2025-10-28T08:55:20+01:00",
+          "tree_id": "19b592c4b1b26554b72fb4d46e5c2c87afac7aea",
+          "url": "https://github.com/No-Smoke/n8n-mcp-enhanced/commit/3f427f952836fd35cb6d054a45d179d88b9cb37b"
+        },
+        "date": 1764809018263,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0136,
+            "range": "0.3096",
+            "unit": "ms",
+            "extra": "73341 ops/sec"
+          }
+        ]
+      }
+    ]
+  }
+}
